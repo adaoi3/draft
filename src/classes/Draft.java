fea6dee5d097e5c10400291cf13f630b125adbe3
Ivan Hribiniuk <gungungogogo@gmail.com>
2023-01-28 18:28:48 +0200

    V1

---
+++ src/classes/Draft.java
@@ -0,0 +1,14 @@
+package classes;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+public class Draft {
+    Version 1
+    public static void main(String[] args) {
+        LocalDateTime time = Timestamp.from(Instant.now()).toLocalDateTime();
+        System.out.println(Timestamp.from(Instant.now()).toLocalDateTime());
+
+    }
+}
