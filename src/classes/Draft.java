59663c8c99fdd079f1698a4740f92079eed6fb35
Ivan Hribiniuk <gungungogogo@gmail.com>
2023-01-28 18:39:53 +0200

    V3

--- src/classes/Draft.java
+++ src/classes/Draft.java
@@ -5,7 +5,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 
 public class Draft {
-    Version 1 (fucked up, must be 2)
+    Version 3
     public static void main(String[] args) {
         LocalDateTime time = Timestamp.from(Instant.now()).toLocalDateTime();
         System.out.println(Timestamp.from(Instant.now()).toLocalDateTime());
